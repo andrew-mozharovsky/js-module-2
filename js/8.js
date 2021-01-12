9eb384a615e33bdadf8fff5132722a132d7bbe41
AndrewMozharovsky <71605765+AndrewMozharovsky@users.noreply.github.com>
2021-01-12 21:57:35 +0200

    start

---
+++ js/8.js
@@ -0,0 +1,7 @@
+// Стартовый код
+const fruits = ['яблоко', 'персик', 'груша', 'банан'];
+// Пиши код ниже этой строки
+const lastElementIndex = fruits.length - 1;
+const lastElement = fruits[lastElementIndex];
+console.log(lastElementIndex);
+console.log(lastElement);
---
+++ js/9.js
@@ -0,0 +1,8 @@
+function getExtremeElements(array) {
+    // Пиши код ниже этой строки
+    
+
+
+    return [array[0], array[array.length - 1]];
+    // Пиши код выше этой строки
+  }
